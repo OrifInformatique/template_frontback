969466158f6a6116f1cbccde1dbfae2eeaf3169b
AdWav <AdrienClaude.Wavelet@formation.orif.ch>
2024-12-11 14:29:00 +0100

    fix: the "Encoding" UserSeeder part works (now a request generate a JWT token)

--- backend/src/main/java/ch/sectioninformatique/template/user/UserSeeder.java
+++ backend/src/main/java/ch/sectioninformatique/template/user/UserSeeder.java
@@ -1,14 +1,19 @@
 package ch.sectioninformatique.template.user;
-    
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserSeeder implements CommandLineRunner {
         
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserSeeder(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -17,9 +22,20 @@ public class UserSeeder implements CommandLineRunner {
 
     private void loadUserData() {
         if (userRepository.count() == 0) {
-            User user1 = new User("john DOE", "john.doe@test.com", "xswqay");
-            User user2 = new User("jane SMITH", "jane.smith@test.com", "qwertz");
-            User user3 = new User("alice JOHNSON", "alice.johnson@test.com", "yaqwsx");
+            User user1 = new User();
+            user1.setFullName("john DOE");
+            user1.setEmail("john.doe@test.com");
+            user1.setPassword(passwordEncoder.encode("xswqay"));
+
+            User user2 = new User();
+            user2.setFullName("jane SMITH");
+            user2.setEmail("jane.smith@test.com");
+            user2.setPassword(passwordEncoder.encode("qwertz"));
+
+            User user3 = new User();
+            user3.setFullName("alice JOHNSON");
+            user3.setEmail("alice.johnson@test.com");
+            user3.setPassword(passwordEncoder.encode("yaqwsx"));
 
             userRepository.save(user1);
             userRepository.save(user2);
