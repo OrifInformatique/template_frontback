4f6d19671f2f1ddcf0214fa56e54492d32b24778
Didier Viret <didier.viret@orif.ch>
2024-12-13 15:41:50 +0100

    Remove the "/api/v1" in the auth API urls

--- backend/src/main/java/ch/sectioninformatique/template/auth/AuthenticationController.java
+++ backend/src/main/java/ch/sectioninformatique/template/auth/AuthenticationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth")
 @RestController
 public class AuthenticationController {
     private final JwtService jwtService;
