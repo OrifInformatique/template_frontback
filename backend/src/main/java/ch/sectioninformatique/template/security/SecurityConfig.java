db84acbf817b25c60ef1ca55a8a5f8acd60c2587
ovsiankina <ovsiankina@proton.me>
2025-01-22 13:50:51 +0100

    docs: SecurityFilter comments added

--- backend/src/main/java/ch/sectioninformatique/template/security/SecurityConfig.java
+++ backend/src/main/java/ch/sectioninformatique/template/security/SecurityConfig.java
@@ -16,11 +16,17 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        // Links accessible to unregistered users
                         .requestMatchers("/oauth2/**", "/login", "/auth/**").permitAll()
                         .anyRequest().permitAll())
+                // Login form using spring web security
+                // TODO: use react for the form instead but keep the logic
+                // in the backend
                 .formLogin(form -> form
+                        // custom login redirection
                         .defaultSuccessUrl("/auth/success", true))
                 .oauth2Login(oauth2 -> oauth2
+                        // oauth2 login redirection
                         .defaultSuccessUrl("/oauth2/success", true));
 
         return http.build();
