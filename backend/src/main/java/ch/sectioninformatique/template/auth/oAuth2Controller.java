50d9c8281381c1e347053df5f81494a85dec6e07
MoDaORIF <david.mostoslavski@formation.orif.ch>
2025-02-06 14:33:19 +0100

    feat(back): oauth2 login controller

--- backend/src/main/java/ch/sectioninformatique/template/auth/oAuth2Controller.java
+++ backend/src/main/java/ch/sectioninformatique/template/auth/oAuth2Controller.java
@@ -1,17 +1,36 @@
 package ch.sectioninformatique.template.auth;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RequestMapping("/oauth2")
 @RestController
 public class oAuth2Controller {
 
     @GetMapping("/success")
-    public String onAzureLoginSuccess() {
-        // TODO: redirect to REACT frontend
-        return "Successful login with azure !";
-    }
-}
+    public ResponseEntity<?> getUserDetails(@AuthenticationPrincipal OAuth2User principal,
+            HttpServletResponse response) {
+        OAuth2AuthenticationToken authentication = (OAuth2AuthenticationToken) SecurityContextHolder.getContext()
+                .getAuthentication();
+        String accessToken = ((OAuth2AuthorizedClient) authentication.getPrincipal()).getAccessToken().getTokenValue();
 
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("user", principal.getAttributes());
+        responseData.put("token", accessToken);
+
+        return ResponseEntity.ok(responseData);
+    }
+
+}
