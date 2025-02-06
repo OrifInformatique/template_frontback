90e77f68bc6335218855e0ee1a730579f338c728
MoDaORIF <david.mostoslavski@formation.orif.ch>
2025-02-06 11:26:31 +0100

    !feat(back): SecurityConfig updated to accept /auth/login request
    SpringSecurity built-in frontend has been removed. React is now the
    default frontend

--- backend/src/main/java/ch/sectioninformatique/template/security/SecurityConfig.java
+++ backend/src/main/java/ch/sectioninformatique/template/security/SecurityConfig.java
@@ -1,32 +1,50 @@
 package ch.sectioninformatique.template.security;
 
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import ch.sectioninformatique.template.jwt.JwtAuthFilter;
+import ch.sectioninformatique.template.user.UserAuthenticationEntryPoint;
+import ch.sectioninformatique.template.user.UserAuthenticationProvider;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
+    private final UserAuthenticationEntryPoint userAuthenticationEntryPoint;
+    private final UserAuthenticationProvider userAuthenticationProvider;
+
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        // Links accessible to unregistered users
-                        .requestMatchers("/oauth2/**", "/login", "/auth/**").permitAll()
-                        .anyRequest().permitAll())
-                // Login form using spring web security
-                // TODO: use react for the form instead but keep the logic
-                // in the backend
-                .formLogin(form -> form
-                        // custom login redirection
-                        .defaultSuccessUrl("/auth/success", true))
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // Exception handling for authentication failures
+                .exceptionHandling(customizer -> customizer.authenticationEntryPoint(userAuthenticationEntryPoint))
+                // Add JWT filter before Basic Authentication
+                .addFilterBefore(new JwtAuthFilter(userAuthenticationProvider), BasicAuthenticationFilter.class)
+                // Disable CSRF (not needed for stateless APIs)
+                .csrf(AbstractHttpConfigurer::disable)
+                // Set session management to stateless (no sessions)
+                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // Configure authorization rules
+                .authorizeHttpRequests((requests) -> requests
+                        // Allow unauthenticated access to login and OAuth2 endpoints
+                        .requestMatchers(HttpMethod.POST, "/auth/login", "/auth/register").permitAll()
+                        .requestMatchers("/auth/oauth2/**").permitAll()
+                        // Require authentication for all other requests
+                        .anyRequest().authenticated())
+                // Configure OAuth2 login
                 .oauth2Login(oauth2 -> oauth2
-                        // oauth2 login redirection
+                        // Redirect to a success URL after OAuth2 login
                         .defaultSuccessUrl("/oauth2/success", true));
 
         return http.build();
