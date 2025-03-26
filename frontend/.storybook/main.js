0b6c8c2cdd279b59a1595b05d31aeedf61ddc5bc
Didier Perroud <didier.perroud@formation.orif.ch>
2025-03-26 11:45:29 +0100

    feat: add storybook to project

---
+++ frontend/.storybook/main.js
@@ -0,0 +1,29 @@
+
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-styling-webpack"
+  ],
+  "framework": {
+    "name": "@storybook/react-webpack5",
+    "options": {}
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.pcss$/,
+      use: ["style-loader", "css-loader", "postcss-loader"],
+    });
+    return config;
+  }
+};
+export default config;
