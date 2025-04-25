710a094d1218699608a40ac420331de043d83797
Didier Perroud <didier.perroud@formation.orif.ch>
2025-04-25 15:16:20 +0200

    feat: improve file input functionality

--- frontend/src/ui/inputs/file/InputFile.jsx
+++ frontend/src/ui/inputs/file/InputFile.jsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const InputFile = ({ id, disabled = false }) => {
+const InputFile = ({
+    id, name, label = null, accept = "", disabled = false, required = false
+}) => {
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        const selectedFile = event.target.files[0];
+        if (selectedFile) {
+            setFile(selectedFile);
+        } else {
+            setFile(null);
+        }
+    }
+
     return (
-        <label htmlFor={id} className="flex gap-2 items-center">
+        <label htmlFor={id} className="flex flex-col gap-2 items-start">
+            {label && <span>{label}</span>}
             <input
                 id={id}
+                name={name}
                 type="file"
+                accept={accept}
+                onChange={handleFileChange}
                 disabled={disabled}
+                required={required}
             />
         </label>
     );
 }
 
 InputFile.propTypes = {
-    
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    accept: PropTypes.string,
+    disabled: PropTypes.bool,
+    required: PropTypes.bool
 }
 
 export default InputFile;
--- frontend/src/ui/inputs/file/InputFile.stories.js
+++ frontend/src/ui/inputs/file/InputFile.stories.js
@@ -7,4 +7,28 @@ export default {
     layout: "fullscreen"
 }
 
-export const Default = {}
+export const WithLabel = {
+    args: {
+        id: "file-1",
+        name: "file-1",
+        label: "Sélectionnez un fichier :",
+        required: true
+    }
+}
+
+export const Disabled = {
+    args: {
+        id: "file-2",
+        name: "file-2",
+        disabled: true
+    }
+}
+
+export const RestrictedFileTypes = {
+    args: {
+        id: "file-3",
+        name: "file-3",
+        label: "Sélectionnez un fichier (.docx, .pdf) :",
+        accept: ".docx, .pdf"
+    }
+}
