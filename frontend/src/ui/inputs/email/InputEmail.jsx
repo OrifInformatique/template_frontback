f6a9d14cf21bc200fcba104f99419e8f20d41396
Didier Perroud <didier.perroud@formation.orif.ch>
2025-04-25 15:16:09 +0200

    feat: improve email input functionality

--- frontend/src/ui/inputs/email/InputEmail.jsx
+++ frontend/src/ui/inputs/email/InputEmail.jsx
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const InputEmail = ({ id, label, disabled = false }) => {
+const InputEmail = ({
+    id, name, label, disabled = false, placeholder = "", required = false
+}) => {
+    const [email, setEmail] = useState("");
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
+
     return (
-        <label htmlFor={id} className="flex gap-2 items-center">
+        <label htmlFor={id} className="flex flex-col gap-2">
             <span>{label}</span>
             <input
                 id={id}
+                name={name}
                 type="email"
+                value={email}
+                onChange={handleEmailChange}
                 disabled={disabled}
+                placeholder={placeholder}
+                required={required}
             />
         </label>
     );
 }
 
 InputEmail.propTypes = {
-    
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    disabled: PropTypes.bool,
+    placeholder: PropTypes.string,
+    requried: PropTypes.bool
 }
 
 export default InputEmail;
--- frontend/src/ui/inputs/email/InputEmail.stories.js
+++ frontend/src/ui/inputs/email/InputEmail.stories.js
@@ -5,7 +5,22 @@ export default {
     component: InputEmail,
     tags: ["autodocs"],
     layout: "fullscreen",
-    args: { label: "Email" }
+    args: { label: "Email :" }
 }
 
-export const Default = {}
+export const Default = {
+    args: {
+        id: "email-1",
+        name: "email-1",
+        placeholder: "email@example.com",
+        required: true
+    }
+}
+
+export const Disabled = {
+    args: {
+        id: "email-2",
+        name: "email-2",
+        disabled: true
+    }
+}
