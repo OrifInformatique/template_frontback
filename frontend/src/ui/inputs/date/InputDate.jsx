929b33d929e319844d595f0f86d1e51307bc2e06
Didier Perroud <didier.perroud@formation.orif.ch>
2025-04-25 15:15:57 +0200

    feat: improve date input functionality

--- frontend/src/ui/inputs/date/InputDate.jsx
+++ frontend/src/ui/inputs/date/InputDate.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const InputDate = ({ id, label, disabled = false }) => {
+const InputDate = ({ id, name, label, disabled = false, required = false }) => {
+    const [selectedDate, setSelectedDate] = useState("");
+
+    const handleDateChange = (event) => {
+        setSelectedDate(event.target.value);
+    }
+
     return (
-        <label htmlFor={id} className="flex gap-2 items-center">
+        <label htmlFor={id} className="flex flex-col gap-2 items-start">
             <span>{label}</span>
             <input
                 id={id}
+                name={name}
                 type="date"
+                value={selectedDate}
+                onChange={handleDateChange}
                 disabled={disabled}
+                required={required}
             />
         </label>
     );
 }
 
 InputDate.propTypes = {
-    
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    disabled: PropTypes.bool,
+    required: PropTypes.bool
 }
 
 export default InputDate;
--- frontend/src/ui/inputs/date/InputDate.stories.js
+++ frontend/src/ui/inputs/date/InputDate.stories.js
@@ -5,7 +5,23 @@ export default {
     component: InputDate,
     tags: ["autodocs"],
     layout: "fullscreen",
-    args: { label: "Date" }
+    args: {
+        label: "Date :"
+     }
 }
 
-export const Default = {}
+export const Default = {
+    args: {
+        id: "date-1",
+        name: "date-1",
+        required: true
+    }
+}
+
+export const Disabled = {
+    args: {
+        id: "date-2",
+        name: "date-2",
+        disabled: true
+    }
+}
