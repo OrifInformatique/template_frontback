0f1cf03d172db175d3083f756dfff23c780fe0c8
Didier Perroud <didier.perroud@formation.orif.ch>
2025-04-25 15:16:35 +0200

    feat: add hidden input

---
+++ frontend/src/ui/inputs/hidden/InputHidden.stories.js
@@ -0,0 +1,15 @@
+import InputHidden from "./InputHidden";
+
+export default {
+    title: "Components/UI/InputHidden",
+    component: InputHidden,
+    tags: ["autodocs"],
+    layout: "fullscreen"
+}
+
+export const Default = {
+    args: {
+        id: "hidden-1",
+        name: "hidden-1"
+    }
+}
