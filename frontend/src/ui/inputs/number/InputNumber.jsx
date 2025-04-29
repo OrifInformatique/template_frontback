9ba8ca941abefea695367d488dad7b31e50ff677
Didier Perroud <didier.perroud@formation.orif.ch>
2025-04-29 13:52:56 +0200

    feat: improve number input functionality (2)

--- frontend/src/ui/inputs/number/InputNumber.jsx
+++ frontend/src/ui/inputs/number/InputNumber.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const InputNumber = ({
     id, name, label = null, min = null, max = null, disabled = false, required = false
 }) => {
+    const [value, setValue] = useState(0);
+
+    const handleNumberChange = (event) => {
+        let inputValue = event.target.value;
+        console.log(inputValue);
+
+        if (inputValue === "") {
+            setValue(min);
+            return;
+        }
+
+        const normalizedValue = inputValue.replace(/^0+(?!$)/, "");
+        const numericValue = Number(normalizedValue);
+
+        if (numericValue < min) {
+            setValue(min);
+        } else if (numericValue > max) {
+            setValue(max);
+        } else {
+            setValue(normalizedValue);
+        }
+    }
+
     return (
         <label htmlFor={id} className="flex flex-col gap-2 items-start">
             {label && <span>{label}</span>}
@@ -13,9 +36,12 @@ const InputNumber = ({
                 type="number"
                 min={min}
                 max={max}
+                value={value}
+                onChange={handleNumberChange}
                 disabled={disabled}
                 required={required}
             />
+            <p>{value}</p>
         </label>
     );
 }
