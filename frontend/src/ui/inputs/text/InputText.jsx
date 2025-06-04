cf14b58be8be4d772efcfee9f76738f33cae0f53
Dervey Dylan <114241438+derveydylan@users.noreply.github.com>
2025-06-04 11:41:21 +0200

    feat: controlled text input

--- frontend/src/ui/inputs/text/InputText.jsx
+++ frontend/src/ui/inputs/text/InputText.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const InputText = ({ id, name, label, disabled = false, required = false }) => {
+const InputText = ({
+    id,
+    name,
+    label,
+    value = null,
+    defaultValue = null,
+    onChangeFunction = null,
+    disabled = false,
+    required = false
+}) => {
+    const [internalValue, setInternalValue] = useState(value ?? defaultValue ?? "");
+
     if (disabled) required = false;
+
+    const handleTextChange = (event) => {
+        if(onChangeFunction)
+            onChangeFunction(event.target.value);
+
+        setInternalValue(event.target.value)
+    }
 
     return (
         <label htmlFor={id} className="flex flex-col gap-2">
@@ -15,6 +33,8 @@ const InputText = ({ id, name, label, disabled = false, required = false }) => {
                 type="text"
                 id={id}
                 name={name}
+                value={internalValue}
+                onChange={handleTextChange}
                 disabled={disabled}
             />
         </label>
@@ -25,6 +45,9 @@ InputText.propTypes = {
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    defaultValue: PropTypes.string,
+    onChangeFunction: PropTypes.func,
     disabled: PropTypes.bool,
     required: PropTypes.bool
 }
