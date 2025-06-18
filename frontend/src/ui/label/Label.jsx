5c45ec5a20a85306455132c1aa950b7ddc1c6a3e
KJouini <Karim.jouini@formation.orif.ch>
2025-06-18 15:36:56 +0200

    Create: Creation of the Label component

---
+++ frontend/src/ui/label/Label.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Label = ({ htmlFor, children, required = false }) => {
+  return (
+    <label htmlFor={htmlFor} className="font-display text-primary font-medium">
+      {required && <span className="text-danger">*</span>} {children}
+    </label>
+  );
+};
+
+Label.propTypes = {
+  htmlFor: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  required: PropTypes.bool,
+};
+
+export default Label;
---
+++ frontend/src/ui/label/Label.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Label from "./Label";
+
+export default {
+  title: "Components/UI/Label",
+  component: Label,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export const Required = () => (
+  <Label htmlFor="file" required>
+    Label
+  </Label>
+);
+
+export const NotRequired = () => (
+  <Label htmlFor="file">
+    Label
+  </Label>
+);
