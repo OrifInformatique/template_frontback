3de5ef2488d6b2e014bf796375bbd35b03e906d7
KJouini <Karim.jouini@formation.orif.ch>
2025-06-19 13:02:01 +0200

    Fix & create: Fixed the component InputCheckbox, created Multicheckbox component

--- frontend/src/ui/inputs/checkbox/InputCheckbox.jsx
+++ frontend/src/ui/inputs/checkbox/InputCheckbox.jsx
@@ -2,46 +2,48 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 
 const InputCheckbox = ({
-    id,
-    name,
-    label,
-    onChange = () => {},
-    defaultChecked = false,
-    disabled = false,
-    required = false
+  id,
+  name,
+  label,
+  onChange = () => {},
+  defaultChecked = false,
+  disabled = false,
+  required = false
 }) => {
-    const [isChecked, setIsChecked] = useState(defaultChecked);
+  const [isChecked, setIsChecked] = useState(defaultChecked);
 
-    if (disabled && !defaultChecked) required = false;
+  if (disabled && !defaultChecked) required = false;
 
-    const handleCheckboxChange = (event) => {
-        setIsChecked(event.target.checked);
-    }
+  const handleCheckboxChange = (event) => {
+    const checked = event.target.checked;
+    setIsChecked(checked);      
+    onChange(event);              
+  };
 
-    return (
-        <label htmlFor={id} className="flex gap-2 items-center">
-            <input
-                type="checkbox"
-                id={id}
-                name={name}
-                checked={isChecked}
-                onChange={onChange}
-                disabled={disabled}
-                required={required}
-            />
-            <span>{label}</span>
-        </label>
-    );
-}
+  return (
+    <label htmlFor={id} className="flex gap-2 items-center">
+      <input
+        type="checkbox"
+        id={id}
+        name={name}
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+        disabled={disabled}
+        required={required}
+      />
+      <span>{label}</span>
+    </label>
+  );
+};
 
 InputCheckbox.propTypes = {
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-    onChange: PropTypes.func,
-    defaultChecked: PropTypes.bool,
-    disabled: PropTypes.bool,
-    required: PropTypes.bool
-}
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+  defaultChecked: PropTypes.bool,
+  disabled: PropTypes.bool,
+  required: PropTypes.bool
+};
 
 export default InputCheckbox;
--- frontend/src/ui/inputs/checkbox/InputCheckbox.stories.js
+++ frontend/src/ui/inputs/checkbox/InputCheckbox.stories.js
@@ -1,32 +1,29 @@
 import InputCheckbox from "./InputCheckbox";
-import { fn } from "@storybook/test";
 
 export default {
-    title: "Components/UI/InputCheckbox",
-    component: InputCheckbox,
-    tags: ["autodocs"],
-    layout: "fullscreen",
-    args: {
-        label: "Checkbox",
-        onChange: (e) => {
-            
-        }
-    }
-}
+  title: "Components/UI/InputCheckbox",
+  component: InputCheckbox,
+  tags: ["autodocs"],
+  layout: "fullscreen",
+  args: {
+    label: "Checkbox",
+    onChange: (e) => console.log("Changement :", e.target.checked)
+  }
+};
 
 export const Default = {
-    args: {
-        id: "checkbox-1",
-        name: "checkbox-1",
-        required: true
-    }
-}
+  args: {
+    id: "checkbox-1",
+    name: "checkbox-1",
+    required: true
+  }
+};
 
 export const Disabled = {
-    args: {
-        id: "checkbox-2",
-        name: "checkbox-2",
-        defaultChecked: true,
-        disabled: true
-    }
-}
+  args: {
+    id: "checkbox-2",
+    name: "checkbox-2",
+    defaultChecked: true,
+    disabled: true
+  }
+};
