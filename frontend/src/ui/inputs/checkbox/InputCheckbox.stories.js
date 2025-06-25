22159f94364ab1aacab26b24e2898f06c7e20b6e
Didier Viret <didier.viret@orif.ch>
2025-06-25 11:28:32 +0200

    Added a disabled checkbox in Storybook

--- frontend/src/ui/inputs/checkbox/InputCheckbox.stories.js
+++ frontend/src/ui/inputs/checkbox/InputCheckbox.stories.js
@@ -12,7 +12,8 @@ export const MultipleOptions = {
     options: [
       { id: "option-1", name: "choices", label: "Option 1" },
       { id: "option-2", name: "choices", label: "Option 2", defaultChecked: true },
-      { id: "option-3", name: "choices", label: "Option 3" }
+      { id: "option-3", name: "choices", label: "Option 3" },
+      { id: "option-4", name: "choices", label: "Option 4", disabled: true }
     ],
     onChange: (selectedIds) => console.log("Sélection :", selectedIds)
   }
