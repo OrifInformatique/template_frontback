9c4e4437a875e52766bb7f8ea9e01430e6862bfb
Didier Viret <didier.viret@orif.ch>
2025-06-26 11:56:47 +0200

    refactor: use <Label> component in InputCheckbox, InputRadio and InputSearch

--- frontend/src/ui/inputs/radio/InputRadio.jsx
+++ frontend/src/ui/inputs/radio/InputRadio.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Label from "../../label/Label";
 
 const InputRadio = ({ id, label, disabled = false }) => {
     return (
-        <label htmlFor={id} className="flex gap-2 items-center">
+        <Label htmlFor={id} className="flex gap-2 items-center">
             <input
                 type="radio"
                 id={id}
                 disabled={disabled}
             />
             <span>{label}</span>
-        </label>
+        </Label>
     );
 }
 
--- frontend/src/ui/inputs/search/InputSearch.jsx
+++ frontend/src/ui/inputs/search/InputSearch.jsx
@@ -4,14 +4,14 @@ import Label from "../../label/Label";
 
 const InputSearch = ({ id, label, disabled = false }) => {
     return (
-        <label htmlFor={id} className="flex gap-2 items-center">
+        <Label htmlFor={id} className="flex gap-2 items-center">
             <span className="text-primary font-medium">{label}</span>
             <input
                 type="search"
                 id={id}
                 disabled={disabled}
             />
-        </label>
+        </Label>
     );
 }
 
