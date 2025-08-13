3cae1af300738821884d0894bb7328f561d0bcc6
YohanKoch <YohanOlivier.Koch@formation.orif.ch>
2025-08-13 14:29:41 +0200

    build(webpack): Add a proxy to devServer wepack config
    Added a proxy to the devServer part of the webpack config to be able to use API's

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,60 +1,66 @@
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
-const APP_ROOT = process.env.APP_ROOT || '/';
+const APP_ROOT = process.env.APP_ROOT || "/";
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
+    mode: "development",
+    entry: path.resolve(__dirname, "src/index.js"),
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[fullhash].bundle.js',
+        path: path.resolve(__dirname, "dist"),
+        filename: "[fullhash].bundle.js",
         publicPath: APP_ROOT,
-        clean: true
+        clean: true,
     },
-    target: 'web',
+    target: "web",
     devServer: {
-        port: process.env.PORT || '4000',
+        port: process.env.PORT || "4000",
         static: {
-            directory: path.join(__dirname, 'public')
+            directory: path.join(__dirname, "public"),
         },
         open: true,
         hot: true,
         liveReload: true,
-        historyApiFallback:true
+        historyApiFallback: true,
+        proxy: [
+            {
+                context: ["/auth/login", "/users"],
+                target: "https://auth.sectioninformatique.ch",
+                changeOrigin: true,
+                secure: false,
+            },
+        ],
     },
     resolve: {
-        extensions: ['.js', '.jsx', '.json'],
+        extensions: [".js", ".jsx", ".json"],
     },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: 'babel-loader',
+                use: "babel-loader",
             },
             {
                 test: /\.(css|pcss)$/i,
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
+                use: ["style-loader", "css-loader", "postcss-loader"],
             },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'index.tmpl.html')
+            template: path.join(__dirname, "src", "index.tmpl.html"),
         }),
         new webpack.DefinePlugin({
-            'process.env': JSON.stringify(dotenv.parsed),
+            "process.env": JSON.stringify(dotenv.parsed),
         }),
         new CopyPlugin({
-            patterns: [
-                "public"
-            ],
-        })
-    ]
+            patterns: ["public"],
+        }),
+    ],
 };
