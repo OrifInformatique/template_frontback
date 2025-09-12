17c155c03f30fa348213db580216d25af82db35d
YohanKoch <YohanOlivier.Koch@formation.orif.ch>
2025-09-12 10:52:06 +0200

    fix: Fixed missing React import in ScrollToTopButton

--- frontend/src/ui/buttons/scroll-to-top/ScrollToTopButton.jsx
+++ frontend/src/ui/buttons/scroll-to-top/ScrollToTopButton.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const ScrollToTopButton = () => {
     const [isVisible, setVisible] = useState(false);
@@ -10,18 +10,18 @@ const ScrollToTopButton = () => {
             } else {
                 setVisible(false);
             }
-        }
+        };
 
-        window.addEventListener("scroll", toggleVisibility);
-        return () => window.removeEventListener("scroll", toggleVisibility);
+        window.addEventListener('scroll', toggleVisibility);
+        return () => window.removeEventListener('scroll', toggleVisibility);
     }, []);
 
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
-            behavior: "smooth"
+            behavior: 'smooth',
         });
-    }
+    };
 
     if (!isVisible) return null;
 
@@ -30,11 +30,23 @@ const ScrollToTopButton = () => {
             onClick={scrollToTop}
             className="fixed bottom-16 right-8 p-3 bg-primary bg-opacity-80 rounded-full transition duration-300 hover:bg-opacity-100 hover:scale-105"
         >
-            <svg className="size-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+            <svg
+                className="size-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
+                />
             </svg>
         </button>
     );
-}
+};
 
 export default ScrollToTopButton;
+
