c0543f4f49a4ad66fc6905627b40445e92a67933
Didier Viret <didier.viret@orif.ch>
2025-11-05 11:18:33 +0100

    refactor: rename ADMIN_TEST to LOCAL_APP_ROLE

--- backend/src/main/java/ch/sectioninformatique/template/user/UserService.java
+++ backend/src/main/java/ch/sectioninformatique/template/user/UserService.java
@@ -39,31 +39,31 @@ public class UserService {
     private final UserMapper userMapper;
 
     /**
-     * Promotes a user to the local admin role.
+     * Promotes a user to the local application example role.
      * This operation:
      * - Verifies the user exists
-     * - Checks if the user is already an manager or admin
-     * - Removes existing roles and assigns the admin role
+     * - Checks if the user has already the local application example role
+     * - Removes existing roles and assigns the local application example role
      *
      * @param userId The ID of the user to promote
      * @return UserDto containing the updated user's information
-     * @throws RuntimeException if the user is not found, already an manager, or the
-     *                          manager role is not found
+     * @throws RuntimeException if the user is not found, already has the role, or the
+     *                          role is not found
      */
-    public UserDto promoteToTestAdmin(Long userId) {
+    public UserDto promoteToLocalAppRole(Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         for (Role role : user.getAppSpecificRoles()) {
-            if (role.getName().equals(RoleEnum.ADMIN_TEST)) {
-                throw new RuntimeException("The user is already a test admin");
+            if (role.getName().equals(RoleEnum.LOCAL_APP_ROLE)) {
+                throw new RuntimeException("The user has already the local app role");
             }
         }
 
-        Role testAdminRole = roleRepository.findByName(RoleEnum.ADMIN_TEST)
-                .orElseThrow(() -> new RuntimeException("ADMIN_TEST role not found"));
+        Role localAppRole = roleRepository.findByName(RoleEnum.LOCAL_APP_ROLE)
+                .orElseThrow(() -> new RuntimeException("LOCAL_APP_ROLE role not found"));
 
-        user.getAppSpecificRoles().add(testAdminRole);
+        user.getAppSpecificRoles().add(localAppRole);
         userRepository.save(user);
         return userMapper.toUserDto(user);
     }
