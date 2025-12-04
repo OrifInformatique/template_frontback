4db1c57704443acbba5b5766fb895c3db1899677
YohanKoch <YohanOlivier.Koch@formation.orif.ch>
2025-12-04 13:43:06 +0100

    feat: Add login navigation to Header component in MainLayout

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,21 +1,33 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 
 import { Header,
          Footer,
          ScrollToTopButton
        } from "@orif-informatique/react-components-library";
+import useAuthStore from "../../authStore";
 
 const MainLayout = () => {
-  return (<>
-      <Header title="App title" logoPath="/images/logo.svg" />
+  const navigate = useNavigate();
+  const accessToken = useAuthStore((s) => s.accessToken);
+  const clearAccessToken = useAuthStore((s) => s.clearAccessToken);
+
+  return (
+    <>
+      <Header
+        title="App title"
+        logoPath="/images/logo.svg"
+        onLogin={() => navigate('/login')}
+        onLogout={() => { clearAccessToken(); navigate('/'); }}
+      />
       {/* <Outlet /> */}
       <div className="flex justify-center items-center font-medium text-4xl text-gray-500 h-96 bg-background">
         C O N T E N T
       </div>
       <ScrollToTopButton onClick={() => {}} />
       <Footer />
-  </>);
+    </>
+  );
 }
 
 export default MainLayout;
