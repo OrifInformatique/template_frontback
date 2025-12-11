889c92f2b761d6f0fae0ddcde49a5f472995e3aa
YohanKoch <YohanOlivier.Koch@formation.orif.ch>
2025-12-11 12:00:14 +0100

    feat: Update logout functionality to clear authentication state

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -9,8 +9,8 @@ import useAuthStore from "../../authStore";
 
 const MainLayout = () => {
   const navigate = useNavigate();
-  const accessToken = useAuthStore((s) => s.accessToken);
-  const clearAccessToken = useAuthStore((s) => s.clearAccessToken);
+  const clearAuth = useAuthStore((s) => s.clearAuth);
+
 
   return (
     <>
@@ -18,7 +18,7 @@ const MainLayout = () => {
         title="App title"
         logoPath="/images/logo.svg"
         onLogin={() => navigate('/login')}
-        onLogout={() => { clearAccessToken(); navigate('/'); }}
+        onLogout={() => { clearAuth(); navigate('/'); }}
       />
       {/* <Outlet /> */}
       <div className="flex justify-center items-center font-medium text-4xl text-gray-500 h-96 bg-background">
