81165022acde9ce1bd33f4695f6e8827555e097c
Chenxi <chenxi.lin.pro@gmail.com>
2026-01-20 16:52:50 +0100

    temporary commit

---
+++ frontend/src/features/progress-report/components/report-bar/ReportBar.jsx
@@ -0,0 +1,19 @@
+/**
+ * ReportBar.jsx
+ *
+ * Reusable progress bar component built div elements and css property "flex-basis".
+ * - Automatically normalizes incoming data.
+ * - Fully responsive thanks to CSS container and the component's own settings.
+ */
+
+import React from "react";
+import "./ReportBar.css";
+
+/**
+ * Main ReportBar component
+ */
+export default function ReportBar() {
+    return (
+        <ReportBar className="reportbar-style" />
+    );
+}
