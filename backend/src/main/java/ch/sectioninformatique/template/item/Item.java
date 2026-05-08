f661d72ba93256acc58b121f838e431aa33c873f
Didier Viret <didier.viret@orif.ch>
2026-05-08 14:57:09 +0200

    refactor:  imports in alphabetical order

--- backend/src/main/java/ch/sectioninformatique/template/item/Item.java
+++ backend/src/main/java/ch/sectioninformatique/template/item/Item.java
@@ -1,13 +1,7 @@
 package ch.sectioninformatique.template.item;
 
-import java.util.Date;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import ch.sectioninformatique.template.user.User;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -17,7 +11,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
+import java.util.Date;
+
 import lombok.Data;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.annotations.UpdateTimestamp;
 
 /**
  * Entity class representing an item in the system.
