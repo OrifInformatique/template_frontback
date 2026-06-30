417069d9276d01dde162cab7d3ddad4682a690a8
Didier Viret <didier.viret@orif.ch>
2026-06-30 11:34:43 +0200

    refactor(backend roles):  add a RoleService layer between RoleController and RoleRepository

--- backend/src/main/java/ch/sectioninformatique/template/security/RoleController.java
+++ backend/src/main/java/ch/sectioninformatique/template/security/RoleController.java
@@ -1,6 +1,5 @@
 package ch.sectioninformatique.template.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/roles")
 public class RoleController {
-    @Autowired
-    private RoleRepository roleRepository;
+    private final RoleService roleService;
 
+    RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    /**
+     * Retrieves all roles in the system.
+     *
+     * @return A ResponseEntity containing the list of all roles
+     */
     @GetMapping("/all")
     public ResponseEntity<?> getRoles() {
-        return ResponseEntity.ok().body(roleRepository.findAll());
+        return ResponseEntity.ok().body(roleService.getAllRoles());
     }
-    
-
-
 }
